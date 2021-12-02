56120f7dc6893490e3f68326b72c014e63ec523c
Genevive Alfirevic <gen@nyble.dev>
2021-12-01 23:21:28 -0600

    Finish day2 challenge one

---
+++ src/bin/day2.rs
@@ -0,0 +1,64 @@
+use std::str::FromStr;
+
+use aoc2021::parse_input_lines;
+
+fn main() {
+    let cmds: Vec<MovementCommand> = parse_input_lines(2).unwrap();
+
+    let (horizontal, depth) =
+        cmds.iter()
+            .fold((0, 0), |(horizontal, depth), command| match command.dir {
+                Direction::Forward => (horizontal + command.units, depth),
+                Direction::Up => (horizontal, depth - command.units),
+                Direction::Down => (horizontal, depth + command.units),
+            });
+
+    println!(
+        "Depth is {} and horizontal position {}. The product of those is {}",
+        horizontal,
+        depth,
+        horizontal * depth
+    );
+}
+
+struct MovementCommand {
+    pub dir: Direction,
+    pub units: isize,
+}
+
+impl FromStr for MovementCommand {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.split_once(' ') {
+            Some((direction, units)) => {
+                let dir = direction.parse()?;
+                let units = units
+                    .parse()
+                    .map_err(|e| format!("Faield to parse '{}' as units: {}", units, e))?;
+
+                Ok(Self { dir, units })
+            }
+            None => Err(format!("String '{}' does not contain a space", s)),
+        }
+    }
+}
+
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+impl FromStr for Direction {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "forward" => Ok(Self::Forward),
+            "up" => Ok(Self::Up),
+            "down" => Ok(Self::Down),
+            _ => Err(format!("'{}' is not a valid direction", s)),
+        }
+    }
+}
